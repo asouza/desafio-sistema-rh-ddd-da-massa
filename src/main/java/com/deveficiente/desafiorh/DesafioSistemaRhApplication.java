23f47efeb6cf866c2e6b554ce173d9f97f0a036f
alberto souza <alots.ssa@gmail.com>
2020-05-30 07:38:39 -0300

    comeco de tudo

---
+++ src/main/java/com/deveficiente/desafiorh/DesafioSistemaRhApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiorh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSistemaRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSistemaRhApplication.class, args);
+	}
+
+}
